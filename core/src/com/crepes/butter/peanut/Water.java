548464e4651afa1b02db6911c1595e39481e1086
PnutButtrCrepes <nathanjkeenan@gmail.com>
2023-03-13 04:26:57 -0400

    Incorporate CachedWaterBlock into Water class

--- core/src/com/crepes/butter/peanut/Water.java
+++ core/src/com/crepes/butter/peanut/Water.java
@@ -724,4 +724,20 @@ public class Water extends Entity
 
 	UP, DOWN, LEFT, RIGHT
     }
+
+    private class CachedWaterBlock extends Entity
+    {
+
+	public boolean looped;
+
+	public CachedWaterBlock(float x, float y, float sizeX, float sizeY)
+	{
+
+	    super(x, y, sizeX, sizeY);
+
+	    addSprite("Water.png", "water");
+
+	    looped = false;
+	}
+    }
 }
