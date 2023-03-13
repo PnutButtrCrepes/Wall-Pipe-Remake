e8e89589d5b1960ed2d3680f22fe0a4bf33519f1
PnutButtrCrepes <nathanjkeenan@gmail.com>
2023-03-13 04:21:45 -0400

    Incorporate WaterDirection and BuildingBlockType into other classes

---
+++ core/src/com/crepes/butter/peanut/blocks/BuildingBlock.java
@@ -0,0 +1,225 @@
+package com.crepes.butter.peanut.blocks;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.crepes.butter.peanut.Entity;
+
+public class BuildingBlock extends Entity
+{
+
+    public BuildingBlockType type;
+
+    public boolean hasUpEntrance;
+    public boolean hasDownEntrance;
+    public boolean hasLeftEntrance;
+    public boolean hasRightEntrance;
+
+    public boolean hasUpExit;
+    public boolean hasDownExit;
+    public boolean hasLeftExit;
+    public boolean hasRightExit;
+
+    public boolean watered;
+    public boolean beingReplaced;
+
+    public boolean visible;
+
+    public BuildingBlock(BuildingBlockType type)
+    {
+
+	super(0f, 0f, 1f, 1f);
+
+	watered = false;
+	beingReplaced = false;
+
+	this.type = type;
+
+	if (type != null)
+	    switch (this.type)
+	    {
+
+	    case CROSS:
+
+		addSprite("Cross.png", "block");
+
+		hasUpEntrance = true;
+		hasDownEntrance = true;
+		hasLeftEntrance = true;
+		hasRightEntrance = true;
+
+		hasUpExit = true;
+		hasDownExit = true;
+		hasLeftExit = true;
+		hasRightExit = true;
+
+		break;
+
+	    case DLELBOW:
+
+		addSprite("DLElbow.png", "block");
+
+		hasUpEntrance = false;
+		hasDownEntrance = true;
+		hasLeftEntrance = true;
+		hasRightEntrance = false;
+
+		hasUpExit = false;
+		hasDownExit = true;
+		hasLeftExit = true;
+		hasRightExit = false;
+
+		break;
+
+	    case DRELBOW:
+
+		addSprite("DRElbow.png", "block");
+
+		hasUpEntrance = false;
+		hasDownEntrance = true;
+		hasLeftEntrance = false;
+		hasRightEntrance = true;
+
+		hasUpExit = false;
+		hasDownExit = true;
+		hasLeftExit = false;
+		hasRightExit = true;
+
+		break;
+
+	    case HSQUIGGLY:
+
+		addSprite("HSquiggly.png", "block");
+
+		hasUpEntrance = false;
+		hasDownEntrance = false;
+		hasLeftEntrance = true;
+		hasRightEntrance = true;
+
+		hasUpExit = false;
+		hasDownExit = false;
+		hasLeftExit = true;
+		hasRightExit = true;
+
+		break;
+
+	    case HSTRAIGHT:
+
+		addSprite("HStraight.png", "block");
+
+		hasUpEntrance = false;
+		hasDownEntrance = false;
+		hasLeftEntrance = true;
+		hasRightEntrance = true;
+
+		hasUpExit = false;
+		hasDownExit = false;
+		hasLeftExit = true;
+		hasRightExit = true;
+
+		break;
+
+	    case ULELBOW:
+
+		addSprite("ULElbow.png", "block");
+
+		hasUpEntrance = true;
+		hasDownEntrance = false;
+		hasLeftEntrance = true;
+		hasRightEntrance = false;
+
+		hasUpExit = true;
+		hasDownExit = false;
+		hasLeftExit = true;
+		hasRightExit = false;
+
+		break;
+
+	    case URELBOW:
+
+		addSprite("URElbow.png", "block");
+
+		hasUpEntrance = true;
+		hasDownEntrance = false;
+		hasLeftEntrance = false;
+		hasRightEntrance = true;
+
+		hasUpExit = true;
+		hasDownExit = false;
+		hasLeftExit = false;
+		hasRightExit = true;
+
+		break;
+
+	    case VSQUIGGLY:
+
+		addSprite("VSquiggly.png", "block");
+
+		hasUpEntrance = true;
+		hasDownEntrance = true;
+		hasLeftEntrance = false;
+		hasRightEntrance = false;
+
+		hasUpExit = true;
+		hasDownExit = true;
+		hasLeftExit = false;
+		hasRightExit = false;
+
+		break;
+
+	    case VSTRAIGHT:
+
+		addSprite("VStraight.png", "block");
+
+		hasUpEntrance = true;
+		hasDownEntrance = true;
+		hasLeftEntrance = false;
+		hasRightEntrance = false;
+
+		hasUpExit = true;
+		hasDownExit = true;
+		hasLeftExit = false;
+		hasRightExit = false;
+
+		break;
+
+	    default:
+		break;
+	    }
+
+	visible = true;
+    }
+
+    @Override
+    public void act(float delta)
+    {
+
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+
+	this.toFront();
+
+	batch.end();
+	batch.begin();
+
+	if (visible)
+	    batch.draw(getSprite("block"), this.getX(), this.getY(), this.getWidth(), this.getHeight());
+    }
+
+    public enum BuildingBlockType
+    {
+
+	CROSS, ULELBOW, URELBOW, DLELBOW, DRELBOW, HSTRAIGHT, VSTRAIGHT, HSQUIGGLY, VSQUIGGLY,
+
+	L_BATHTUB, R_BATHTUB,
+
+	DARK_ULELBOW, DARK_URELBOW, DARK_DLELBOW, DARK_DRELBOW, U_VSTRAIGHT, D_VSTRAIGHT,
+
+	TELEPORT_CROSS,
+
+	DLELBOW_50, DRELBOW_50,
+
+	SINK
+    }
+}
---
+++ core/src/com/crepes/butter/peanut/blocks/WaterEmitter.java
@@ -0,0 +1,85 @@
+package com.crepes.butter.peanut.blocks;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.crepes.butter.peanut.GameScene;
+
+public class WaterEmitter extends BuildingBlock
+{
+
+    public GameScene gameScene;
+
+    public int typeIndex;
+
+    public WaterEmitter(GameScene gameScene)
+    {
+
+	super(null);
+
+	this.gameScene = gameScene;
+
+	this.watered = true;
+
+	init((int) (Math.random() * 12 + 3), (int) (Math.random() * 9 + 3), 1, 1);
+
+	this.typeIndex = (int) (Math.random() * 2);
+
+	if (typeIndex == 0)
+	{
+	    addSprite("LWaterEmitter.png", "block");
+	    this.hasLeftExit = true;
+
+	} else
+	{
+	    addSprite("RWaterEmitter.png", "block");
+	    this.hasRightExit = true;
+	}
+
+	this.hasUpEntrance = false;
+	this.hasDownEntrance = false;
+	this.hasRightEntrance = false;
+	this.hasLeftEntrance = false;
+
+	gameScene.bfManager.addBlock(this, (int) this.getUnscaledX(), (int) this.getUnscaledY());
+    }
+
+    public void reset()
+    {
+
+	setPosition((int) (Math.random() * 12 + 3), (int) (Math.random() * 9 + 3));
+
+	this.typeIndex = (int) (Math.random() * 2);
+
+	if (typeIndex == 0)
+	{
+
+	    addSprite("LWaterEmitter.png", "block");
+	    this.hasLeftExit = true;
+	    this.hasRightExit = false;
+
+	} else
+	{
+
+	    addSprite("RWaterEmitter.png", "block");
+	    this.hasRightExit = true;
+	    this.hasLeftExit = false;
+	}
+
+	gameScene.bfManager.addBlock(this, (int) this.getUnscaledX(), (int) this.getUnscaledY());
+    }
+
+    @Override
+    public void act(float delta)
+    {
+
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+
+	batch.end();
+	batch.begin();
+
+	batch.draw(getSprite("block"), this.getX(), this.getY(), this.getWidth(), this.getHeight());
+    }
+}
